15a383726d6c68992b29954dd58c549f91c82011
Sai Gopinath <sainath.gopinath@gmail.com>
2026-02-12 23:10:37 -0500

    React_Refresher: Creating_React_Elements

--- 2_Creating_React_Elements/react-project/src/App.jsx
+++ 2_Creating_React_Elements/react-project/src/App.jsx
@@ -3,33 +3,46 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+let language = "Javascript";
+let moon = "🌙";
+/*
+function Header(props){
+  //console.log(props);
+  const name = props.name;
+  return (
+    <header>
+      <h1>{name}'s kitchen</h1>
+      <p>Copyright {props.year}</p>
+    </header>
+  );
+}
+*/
+
+function Header({name, year}){ //destructuring props
+  //console.log(props);
+  //const name = name;
+  return (
+    <header>
+      <h1>{name}'s kitchen</h1>
+      <p>Copyright {year}</p>
+    </header>
+  );
+}
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>My React Project!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    //<h1>Hello React!</h1>
+    //<h1>Hello {language.toUpperCase()} {moon}!!!</h1>
+    <div>
+      <Header name="Alex" year={new Date().getFullYear()} />
+    <main>
+      <h2>We serve the most delicious food around</h2>
+    </main>
+    </div>
+  );
 }
 
 export default App
