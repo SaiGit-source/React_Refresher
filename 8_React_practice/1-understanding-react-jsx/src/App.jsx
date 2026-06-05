170173061e87e9d3d6f481c52f6612ada837d088
Sai Gopinath <sainath.gopinath@gmail.com>
2026-06-05 12:29:22 -0400

    React_Refresher: Forms_EventHandling example

--- 8_React_practice/1-understanding-react-jsx/src/App.jsx
+++ 8_React_practice/1-understanding-react-jsx/src/App.jsx
@@ -28,6 +28,65 @@ function AppleCounter(){
   );
 }
 
+//4.example of forms
+function NameForm() {
+  const [name, setName] = useState(''); // value always matches the state and it is called as a controlled input
+  const [submittedNames, setSubmittedNames] = useState([]); // state to store the submitted names
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // prevent the default form submission behavior
+    if (name) {
+      setSubmittedNames([...submittedNames, name]); // add the current name to the list of submitted names, ...longList spreaded into separate values
+      setName(''); // clear the input field after submission
+    }
+  };
+
+  return (
+    <div>
+      <h3>Enter your name:</h3>
+      <input
+        type="text"
+        placeholder="Your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <p>Your typed: {name}</p>
+      <button onClick={handleSubmit}>Submit</button>
+
+      {
+        submittedNames.length > 0 && (
+          <div>
+            <h4>Submitted Names:</h4>
+            <ul>
+              {submittedNames.map((submittedName, index) => (
+                <li key={index}>{submittedName}</li>
+              ))}      
+            </ul>
+          </div>
+        )
+  }
+    </div>
+  );
+}
+
+
+//5.example of Event Handling
+function SecretMessage() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClick = () => {
+    setIsVisible(!isVisible); // toggle the visibility of the secret message
+  };
+
+  return (
+    <div>
+      <h3>Click to reveal secret message</h3>
+      <button onClick={handleClick}>Reveal Message</button>
+      {isVisible && <p>This is a secret message!</p>}
+    </div>
+  );
+}
+
 function App() { // main component also a functional component that renders the WelcomeMessage component inside a div element
 
   return (
@@ -38,6 +97,10 @@ function App() { // main component also a functional component that renders the 
       <UserName name="Jane Smith" />
       Use State Example:
       <AppleCounter />
+      Form Example:
+      <NameForm />
+      Event Handling Example:
+      <SecretMessage />
     </div>
   )
 }
